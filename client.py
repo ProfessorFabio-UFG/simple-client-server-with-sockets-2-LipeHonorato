3bb9cbc225d2d7be45fdfbd6f1a8fc2ca9cad618
Felipe Honorato <99209704+LipeHonorato@users.noreply.github.com>
2025-03-30 21:44:32 -0300

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,19 @@ from constCS import * #-
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
+
+#Imprime na tela para o client indicar o cálculo desejado
+valor1 = input("Digite o valor 1: ")
+valor2 = input("Digite o valor 2: ")
+op =  input("Digite a operação (sum, sub ou div): ")
+
+#tranforma o calculo em uma string
+calculo = valor1 + " " + valor2 + " " + op
+print(calculo)
+
+s.send(str.encode(calculo))  # send some data
 data = s.recv(1024)     # receive the response
+
 print (bytes.decode(data))            # print the result
+
 s.close()               # close the connection
